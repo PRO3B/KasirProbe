bcad9108317c318475a3ac3bb276a32fa6b8658e
PROBE <mi3070928@gmail.com>
2025-09-28 21:19:50 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
     implementation(libs.coil.kt)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.core)
-    implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation("androidx.compose.material:material-icons-extended:1.6.1")
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
